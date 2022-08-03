ed1bbefa820c267dc8ef8f6030f046f02c2c1de8
msilot1001 <msilot1001@gmail.com>
2022-08-03 20:57:50 +0900

    활공기 상승기 추가염

--- src/EventListener.java
+++ src/EventListener.java
@@ -24,6 +24,44 @@ public class EventListener implements Listener{
                 Player player = (Player) passenger;
                 ItemStack item = player.getItemInHand();
 
+                if(item != null && item.getType() == Material.LIME_BANNER && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals("상승기")) {
+                    Double multiplier = 0.5D;
+                    boat.setVelocity(new Vector(boat.getLocation().getDirection().multiply(multiplier).getX(), 0.09D, boat.getLocation().getDirection().multiply(multiplier).getZ()));
+                    Location loc = boat.getLocation();
+                    player.getWorld().playSound(loc, Sound.ENTITY_WOLF_GROWL, 0.4F, 0);
+                    player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, loc.getX(), loc.getY() + 1, loc.getZ(), 0);
+                    player.getWorld().spawnParticle(Particle.END_ROD, loc.getX(), loc.getY() + 1, loc.getZ(), 0);
+
+                    // 점수 부과
+                    ScoreboardManager manager = Bukkit.getScoreboardManager();
+                    Scoreboard board = manager.getMainScoreboard();
+                    Objective obj = board.getObjective("boosttime");
+
+                    assert obj != null;
+                    Score score = obj.getScore(player.getName());
+
+                    score.setScore((int) (score.getScore() + Math.ceil(multiplier)));
+                }
+
+                if(item != null && item.getType() == Material.WHITE_BANNER && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals("활공기")) {
+                    Double multiplier = 0.5D;
+                    boat.setVelocity(new Vector(boat.getLocation().getDirection().multiply(multiplier).getX(), -0.04D, boat.getLocation().getDirection().multiply(multiplier).getZ()));
+                    Location loc = boat.getLocation();
+                    player.getWorld().playSound(loc, Sound.ENTITY_WOLF_GROWL, 0.4F, 0);
+                    player.getWorld().spawnParticle(Particle.FIREWORKS_SPARK, loc.getX(), loc.getY() + 1, loc.getZ(), 0);
+                    player.getWorld().spawnParticle(Particle.END_ROD, loc.getX(), loc.getY() + 1, loc.getZ(), 0);
+
+                    // 점수 부과
+                    ScoreboardManager manager = Bukkit.getScoreboardManager();
+                    Scoreboard board = manager.getMainScoreboard();
+                    Objective obj = board.getObjective("boosttime");
+
+                    assert obj != null;
+                    Score score = obj.getScore(player.getName());
+
+                    score.setScore((int) (score.getScore() + Math.ceil(multiplier)));
+                }
+
                 if(item != null && item.getType() == Material.FEATHER && player.getInventory().getItemInHand().getItemMeta().getDisplayName().equals("메인로터")) {
                     int multiplier = 10;
                     boat.setVelocity(new Vector(0, 0.4D, 0));
